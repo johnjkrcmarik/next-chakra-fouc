0f0c1369e703799d7bd3b986359dd5e91e2e017b
Griko Nibras <git@griko.id>
2022-01-04 03:09:52 +0700

    feat: add emotion cache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 
+import emotionCache from "@/lib/emotion-cache";
+
 import { ChakraProvider } from "@chakra-ui/react";
+import { CacheProvider } from "@emotion/react";
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <CacheProvider value={emotionCache}>
+      <ChakraProvider resetCSS>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </CacheProvider>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,30 @@
 import * as React from "react";
 
+import emotionCache from "@/lib/emotion-cache";
+
 import { ColorModeScript } from "@chakra-ui/react";
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import Document, { DocumentContext, Head, Html, Main, NextScript } from "next/document";
+
+const { extractCritical } = createEmotionServer(emotionCache);
 
 export default class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = extractCritical(initialProps.html);
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <style
+          key="emotion-css"
+          dangerouslySetInnerHTML={{ __html: styles.css }}
+          data-emotion-css={styles.ids.join(" ")}
+        />,
+      ],
+    };
+  }
+
   render() {
     return (
       <Html lang="en">
